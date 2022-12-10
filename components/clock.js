de001758d1c8f79c12abebbb45d564558171373c
Robin PAUTIGNY <rob1@192.168.1.13>
2022-12-10 12:11:27 +0100

    💄 adding css files 💄

---
+++ components/clock.js
@@ -0,0 +1,24 @@
+import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react';
+
+function Clock() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const timeString = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+  return (
+    <div className={styles.clock}>
+     Currently {timeString} in Paris.🇫🇷
+    </div>
+  );
+}
+
+export default Clock;
+
