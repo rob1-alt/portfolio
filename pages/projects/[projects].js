b04a55838c5889fc4548bb7cad068b83fb6ba778
Robin PAUTIGNY <rob1@192.168.1.13>
2022-12-10 18:30:42 +0100

    yet bad code

--- pages/projects/[projects].js
+++ pages/projects/[projects].js
@@ -1,38 +1,38 @@
-import Head from 'next/head'
+// import Head from 'next/head'
+// import content from '../../portfolio.content.json';
 
 
-
-const Details = ({page}) => {
-    return (
+// const Details = ({page}) => {
+//     return (
         
-        <div>
-            <Head>
-                <title>{page.title}</title>
-                <meta name="description" content="Creative boy, who aims to undertake and bring a solution for tomorrow " />
-                <meta property="og:image" content="../src/img"/>
+//         <div>
+//             <Head>
+//                 <title>{page.title}</title>
+//                 <meta name="description" content="Creative boy, who aims to undertake and bring a solution for tomorrow " />
+//                 <meta property="og:image" content="../src/img"/>
 
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Header/>
-            <h1></h1>
-        </div>
-    )
+//                 <link rel="icon" href="/favicon.ico" />
+//             </Head>
+//             <Header/>
+//             <h1></h1>
+//         </div>
+//     )
 
-}
+// }
 
-export async function getStaticPaths() {
-    const paths = content.pages.map(page => {
-      const slug = page.path.split('/').slice(1);
-      return {params: {slug}};
-    });
-    return {paths, fallback: true};
-  }
+// export async function getStaticPaths() {
+//     const paths = content.pages.map(page => {
+//       const slug = page.path.split('/').slice(1);
+//       return {params: {slug}};
+//     });
+//     return {paths, fallback: true};
+//   }
   
-  export async function getStaticProps({params}) {
-    const currentPath = `/${params.slug.join('/')}`;
-    const page = content.pages.find(page => page.path === currentPath) || {notfound: true};
-    return {props: {page}};
-  }
+//   export async function getStaticProps({params}) {
+//     const currentPath = `/${params.slug.join('/')}`;
+//     const page = content.pages.find(page => page.path === currentPath) || {notfound: true};
+//     return {props: {page}};
+//   }
 
 
-    export default Details
+//     export default Details
